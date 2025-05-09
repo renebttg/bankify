8d58c55802f21de796f21473a3961af4e124b8c9
renebttg <renebattaglia@hotmail.com>
2025-05-09 03:11:43 -0300

    Initial commit

---
+++ src/main/java/com/example/bankify/BankifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bankify/BankifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
